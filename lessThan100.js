b774eeabc69cc60e82da7e94bcfaf030db6bd8a6
chriswebdv <chriswebdv@gmail.com>
2024-06-10 18:05:34 -0600

    Add if statement to determine if sum is less of greater than 100

--- lessThan100.js
+++ lessThan100.js
@@ -1,3 +1,13 @@
+let num1;
+let num2;
+let sum = num1 + num2;
+
 function lessThan100(a, b) {
-  return;
+  if (sum < 100) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+console.log(lessThan100(100, 15));
