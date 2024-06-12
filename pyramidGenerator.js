26827ceafc5edce2eb95dec337d1f678cdebfb30
chriswebdv <chriswebdv@gmail.com>
2024-06-12 17:34:34 -0600

    apply .repeat method to the character string inside the for loop

--- pyramidGenerator.js
+++ pyramidGenerator.js
@@ -3,7 +3,13 @@ const count = 8;
 const rows = [];
 
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(i); // adding i to the rows array
+  rows.push(character.repeat(i)); // adding # to the rows array
 }
 
 let result = "";
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
