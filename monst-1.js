22ba79dece7a675fe3a1651c74850c176fbea1f6
chriswebdv <chriswebdv@gmail.com>
2024-07-17 21:00:07 -0600

    create a sum variable and add all the of variable to it.

--- monst-1.js
+++ monst-1.js
@@ -4,6 +4,8 @@ let third = 5;
 let fourth = 7;
 let fifth = 8;
 
+let sum = first + second + third + fourth + fifth;
+
 second = 3;
 
-console.log(first + second + third + fourth + fifth);
+console.log(sum);
