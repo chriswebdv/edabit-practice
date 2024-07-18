846537d0673752c5c38fa644c963060678d0dd95
chriswebdv <chriswebdv@gmail.com>
2024-07-17 20:57:35 -0600

    create variables and add them together

--- monst-1.js
+++ monst-1.js
@@ -1,6 +1,9 @@
 let first = 1;
 let second = 2;
+let third = 5;
+let fourth = 7;
+let fifth = 8;
 
 second = 3;
 
-console.log(first + second);
+console.log(first + second + third + fourth + fifth);
