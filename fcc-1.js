defd3c3df1fe49d2a77b7f843a470a434923689f
chriswebdv <chriswebdv@gmail.com>
2024-07-24 18:46:38 -0600

    change variable contents, declared and assinged a new variable using came case. Log it to the screen

--- fcc-1.js
+++ fcc-1.js
@@ -1,3 +1,8 @@
 // reviewing the first excercise set of Free Code Camp Curriculum
 let character = "Hello";
 console.log(character);
+
+character = "World";
+let secondCharacter;
+secondCharacter = character;
+console.log(secondCharacter);
