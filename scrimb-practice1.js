d5f62938f1dac5fbad6dec5d7ff2e9bc52e6a62c
chriswebdv <chriswebdv@gmail.com>
2024-07-30 07:07:00 -0600

    run code to test if correct

--- scrimb-practice1.js
+++ scrimb-practice1.js
@@ -39,9 +39,9 @@ remove1Point();
 console.log(myPoints);
 
 // Try to predict what each of the lines will log out
-console.log("2" + 2); //
-console.log(11 + 7); //
-console.log(6 + "5"); //
-console.log("My points: " + 5 + 9); //
-console.log(2 + 2); //
-console.log("11" + "14"); //
+console.log("2" + 2); // "22"
+console.log(11 + 7); // 18
+console.log(6 + "5"); // "65"
+console.log("My points: " + 5 + 9); // "59"
+console.log(2 + 2); // 4
+console.log("11" + "14"); // "1114"
