af9799e530c11d8e4fb78c6b1522a6f4e6a0f5a1
chriswebdv <chriswebdv@gmail.com>
2024-07-30 07:04:46 -0600

    check strings

--- scrimb-practice1.js
+++ scrimb-practice1.js
@@ -37,3 +37,11 @@ remove1Point();
 
 // Call the functions to that the line below logs out 10
 console.log(myPoints);
+
+// Try to predict what each of the lines will log out
+console.log("2" + 2); //
+console.log(11 + 7); //
+console.log(6 + "5"); //
+console.log("My points: " + 5 + 9); //
+console.log(2 + 2); //
+console.log("11" + "14"); //
