0e3833c5ae584f5003193348a2041f3aa8aafbab
chriswebdv <chriswebdv@gmail.com>
2024-07-30 06:58:32 -0600

    create a function

---
+++ scrimb-practice1.js
@@ -0,0 +1,17 @@
+let firstName = "Tanko";
+let lastName = " Rice";
+let fullName = firstName + lastName;
+
+console.log(fullName);
+
+let personName = "Linda";
+let greeting = "Hi there";
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+let hiThere = () => {
+  greet = greeting + ", " + personName + "!";
+  console.log(greet);
+};
+
+hiThere();
