ffe7b612def57ce3980b632b5fb1eab59af75f30
chriswebdv <chriswebdv@gmail.com>
2024-08-07 18:17:27 -0600

    log the results to the screen

--- random-nums.js
+++ random-nums.js
@@ -1,4 +1,8 @@
 let x = Math.floor(Math.random() * 100 + 1);
 let y = Math.floor(Math.random() * 50 + 1);
 
-console.log(y);
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
